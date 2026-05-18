5512b9fb3291ba82ad72ec5fd0d8e3de77f93703
Kokila <kokilaabeysinghe01@gmail.com>
2026-05-18 19:08:37 +0530

    Alert example

--- src/test/java/AlertExample.java
+++ src/test/java/AlertExample.java
@@ -1,6 +1,10 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -11,8 +15,47 @@ public class AlertExample {
     public void dropDownTestPage() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.leafground.com/alert.xhtml");
 
     }
+
+    @Test
+    public void alertBox()throws InterruptedException{
+        //Alert simple dialog
+        WebElement alertBox = driver.findElement(By.id("j_idt88:j_idt91"));
+        alertBox.click();
+        Thread.sleep(3000);
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+    //Alert confirm Dialog
+
+        WebElement confirmBox = driver.findElement(By.id("j_idt88:j_idt93"));
+        confirmBox.click();
+        Alert alert1  = driver.switchTo().alert();
+        Thread.sleep(3000);
+        alert1.dismiss();
+
+        //Alert (Prompt Dialog)
+        WebElement promtBox = driver.findElement(By.id("j_idt88:j_idt104"));
+        promtBox.click();
+        Alert alert2  = driver.switchTo().alert();
+        Thread.sleep(3000);
+        String alertText = alert2.getText();
+        System.out.println("Alert text is:"+ alertText);
+        alert2.sendKeys("My name is Kokila");
+        alert2.accept();
+
+
+
+
+    }
+
+
+
+
+
+
+
 
 }
